ffba7a197b9bf5f0f162328010cf681b4d33b07b
Douglas Marcolino Pereira <doug_marcolino@hotmail.com>
2023-08-29 22:22:02 -0300

    styling container and todo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Todo from "./components/Todo/Todo";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl overline text-pink-600">Hello world! saaa</h1>
-    </>
+    <div className="bg-slate-800 w-full h-[100vh] flex items-center justify-center">
+      <Todo />
+    </div>
   );
 }
 
--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,5 +1,12 @@
 const Todo: React.FC = () => {
-  return <div>hello todo</div>;
+  return (
+    <div>
+      <div className="bg-gradient-to-r from-sky-500 to-indigo-500 w-[400px] h-20 rounded-lg drop-shadow-md mb-4 flex flex items-center justify-center">
+        <h1 className="text-white font-medium text-xl">ToDo SOLID</h1>
+      </div>
+      <div className="bg-gradient-to-r from-white to-indigo-200 h-[400px] w-[400px] rounded-lg drop-shadow-md"></div>
+    </div>
+  );
 };
 
 export default Todo;
